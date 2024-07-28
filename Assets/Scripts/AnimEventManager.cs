389be61a27e8ba3478bbf469ec9f05b1dc5f2702
connorwright1122 <98538008+connorwright1122@users.noreply.github.com>
2024-07-27 21:33:58 -0400

    fixed particle spray spawn

--- Assets/Scripts/AnimEventManager.cs
+++ Assets/Scripts/AnimEventManager.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class AnimEventManager : MonoBehaviour
 {
     public Transform _particleSpawnLocation;
+    public Transform _particleRotation;
     public ParticleSystem _particleSystem;
     public FlaskManager _flaskManager;
 
     public void PlayParticleSystem()
     {
         _particleSystem.transform.position = _particleSpawnLocation.position;
+        //_particleSystem.transform.localRotation = Quaternion.Euler(_particleRotation.transform.forward);
+        //_particleSystem.transform.rotation = Quaternion.Euler(_particleRotation.transform.TransformDirection(forward));
+        //_particleSystem.transform.rotation = _particleRotation.transform.localRotation; // straight up
+        _particleSystem.transform.rotation = _particleRotation.transform.rotation;
         _particleSystem.Play();
     }
 
